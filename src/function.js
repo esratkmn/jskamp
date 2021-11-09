d271eab5263d12ae4e40f38fe7e90624d9f5c719
esratkmn <75122908+esratkmn@users.noreply.github.com>
2021-11-09 22:38:14 +0300

    fonksiyon

---
+++ src/function.js
@@ -0,0 +1,91 @@
+// function addToCart(productName="elma" ,quantity="20"){
+//     console.log("sepete eklendi! : ürün : " +productName + " adet :" +quantity)
+// }
+// addToCart()
+// addToCart(10)
+// addToCart("karpuz")
+
+//  let sayHello = ()=>{
+//      console.log("sayHello")
+//  }
+//  sayHello()
+
+//  let sayHello2= function(){
+//      console.log("sayHello2")
+//  }
+//  sayHello2()
+
+// function addToCart2(productName,quantity,unitPrice){
+//   console.log("ürün :" +productName ,"miktar :"+quantity,"Fiyat ="+unitPrice)
+  
+// }
+// addToCart2("Elma",2,10)
+// addToCart2("Karpuz",3,15)
+// addToCart2("Limon",4,14)
+
+// let product1={productName:"Elma",unitPrice:10,quantity:5}
+
+// function addToCart3(product){
+//     console.log("ürün:" +product.productName)
+//     console.log("fiyat:" +product.unitPrice)
+//     console.log("adet:" +product.quantity)
+// }
+// addToCart3(product1)
+
+// let product2={productName:"Elma",unitPrice:10,quantity:5}
+// let product3={productName:"Elma",unitPrice:10,quantity:5}
+// product2=product3
+// product2.productName="karpuz"
+// console.log(product3.productName)
+
+// function addToCart4(products){
+//     console.log(products)
+// }
+// let products=[
+//     {productName:"Elma",unitPrice:10,quantity:5},
+//     {productName:"Karpuz",unitPrice:10,quantity:5},
+//     {productName:"Limon",unitPrice:10,quantity:5}
+// ]
+// addToCart4(products)
+
+// function add(number1,number2){
+//     console.log(number1+number2)
+// }
+// add(20,30)
+
+// function add(...numbers){
+//     let total=0
+//     for(let i=1;i<numbers.length;i++)
+//     total=total+numbers[i]
+//     console.log(total)
+// }
+
+// // add(20,30)
+// // add(20,30,40)
+// // add(20,30,40,50)
+
+// let numbers=[100,30,663,23,90]
+// // console.log(Math.max(...numbers))
+
+// let [ıcAnadolu,marmara,karadeniz,[ıcAnadoluSehirleri]]=[
+//     {name:"İç Anadolu", population:"30M"},
+//     {name:"Marmara",population:"20M"},
+//     {name:"Karadeniz",population:"10M"},
+//     [
+//         ["Ankara","Konya"],
+//         ["İstanbul","Bursa"],
+//         ["Sinop","Trabzon"]
+//     ]
+// ]
+// console.log(ıcAnadolu)
+// console.log(marmara.population)
+// console.log(karadeniz)
+// console.log(ıcAnadoluSehirleri)
+
+// let newProductName,newUnitPrice,newQuantity
+//     ({productName:newProductName,unitPrice:newUnitPrice,quantity:newQuantity}={
+//         productName:"Elma",unitPrice:"10",quantity:"5"
+//     })
+// console.log(newProductName)
+// console.log(newUnitPrice)
+// console.log(newQuantity)
